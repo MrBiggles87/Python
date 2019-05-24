0bf41f83e39db7efebaf4f31723f49fdc6ef0299
MrBiggles87 <garry.adams@nationwide.co.uk>
2019-05-24 10:05:57 +0100

    First Python Commands

---
+++ ResultsPercentage.py
@@ -0,0 +1,17 @@
+Physics=80
+Chemistry=100
+Math=100
+Total=Physics+Chemistry+Math
+Per=Total*100/450
+print(" ")
+print("The Quiz Results are")
+print("....................")
+print("Physics:",Physics)
+print("Chemistry:",Chemistry)
+print("Math:",Math)
+print(" ")
+print("Total Results")
+print(" ")
+print(Total)
+print(" ")
+print("Percentage:",Per,"%")

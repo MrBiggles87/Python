39566e556a10637287882b222d42b3aa1ff185c4
MrBiggles87 <garry.adams@nationwide.co.uk>
2019-05-24 12:21:00 +0100

    Example of IF with temperature

---
+++ temperature.py
@@ -0,0 +1,10 @@
+temp=int(input("Please Enter Todays Temperature: "))
+if temp>25:
+	print(" ")
+	print("It is hot today!")
+if temp<25:
+	print(" ")
+	print("It is cold today!")
+if temp==25:
+	print(" ")
+	print("It is warm today!")

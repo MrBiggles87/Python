05f2c5c71f873852d1eb2e6db69e1406a2d143c1
MrBiggles87 <garry.adams@nationwide.co.uk>
2019-05-24 11:30:36 +0100

    Python and Integers

---
+++ Textincommands.py
@@ -0,0 +1,7 @@
+phy=input("Enter Physics Number:")
+chem=input("Enter Chemistry Number:")
+math=input("Enter Maths Number:")
+total=int(phy)+int(chem)+int(math)
+per=total*100/850
+print("The Result is:",total)
+print("The Percent is:",per)

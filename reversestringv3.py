266fa5cf4b20272699fe87dd80bc7225ffbd31db
MrBiggles87 <garry.adams@nationwide.co.uk>
2019-05-30 12:49:19 +0100

    UPDATED WORKING REVERSING OF A STRING

--- reversestringv3.py
+++ reversestringv3.py
@@ -1,9 +1,20 @@
-def reversestring(message):
-	message=input("Please enter a message: ")
-    newstring = " "
-    index=len(message)
-    	while index:
-    		index-=1                    
-    		newstring += a_string[index]
-   		return newstring
+def reverse(word):
+	newword=""
+	index=len(word)-1
+	while index>=0:
+		newword=newword+word[index]
+		index-=1
+	return newword
 
+
+message=input("Enter Any Message: ")
+newmessage=""
+word=""
+for ch in message:
+	if ch==" ":
+		newmessage+=(reverse(word)+" ")
+		word=""
+	else:
+		word+=ch
+newmessage+=(reverse(word)+" ")
+print(newmessage)
